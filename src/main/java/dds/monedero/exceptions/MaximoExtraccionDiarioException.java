4badc76b7e0c190c6b148219579156a1a6f6ca26
SimonCutreraUTN <scutrera@frba.utn.edu.ar>
2025-05-03 23:56:43 -0300

    más code smells

--- src/main/java/dds/monedero/exceptions/MaximoExtraccionDiarioException.java
+++ src/main/java/dds/monedero/exceptions/MaximoExtraccionDiarioException.java
@@ -3,5 +3,5 @@ package dds.monedero.exceptions;
 public class MaximoExtraccionDiarioException extends RuntimeException {
   public MaximoExtraccionDiarioException(String message) {
     super(message);
-  }
+  } //lazy class
 }
